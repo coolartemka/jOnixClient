c30f6d77a354cecf36dd4b1e262d326f7d9ea479
Artem Moskalev <artem.moskalev@metropolia.fi>
2013-11-14 16:10:03 +0200

    internal message resend

--- src/main/java/fi/metropolia/ereading/Registration.java
+++ src/main/java/fi/metropolia/ereading/Registration.java
@@ -13,17 +13,17 @@ public class Registration {
 
 	private String login;
 	private String password;
-	@ManagedProperty(value="#{navigation}")
-	private Navigation navigation;
+	@ManagedProperty(value="#{clientSession}")
+	private UserSession clientSession;
 	
 	public void register(ActionEvent event) {
 		DBCollection usersCollection = MongoResource.getClient().getDB("jOnix").getCollection("users");
 		DBObject user = usersCollection.findOne(new BasicDBObject("login", login));
 		if(user == null) {
 			usersCollection.insert(new BasicDBObject("login", login).append("password", password));
-			navigation.setLogged(true);
-			navigation.setLogin(login);
-			navigation.setPage("home");
+			clientSession.setLogged(true);
+			clientSession.setLogin(login);
+			clientSession.setPage("home");
 		} else {
 			FacesContext.getCurrentInstance()
 				.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Such user already exists", "Login taken"));
@@ -36,13 +36,13 @@ public class Registration {
 			FacesContext.getCurrentInstance()
 			.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Wrong Credentials", "Login-Passowrd pair wrong"));
 		} else {
-			navigation.setLogged(true);
-			navigation.setPage("home");
-			navigation.setLogin(login);
+			clientSession.setLogged(true);
+			clientSession.setPage("home");
+			clientSession.setLogin(login);
 		}		
 	}
 	public String proceed() {
-		if(navigation.isLogged()) {
+		if(clientSession.isLogged()) {
 			return "personal";
 		} else {
 			return null;
@@ -61,11 +61,11 @@ public class Registration {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public Navigation getNavigation() {
-		return navigation;
+	public UserSession getClientSession() {
+		return clientSession;
 	}
-	public void setNavigation(Navigation navigation) {
-		this.navigation = navigation;
+	public void setClientSession(UserSession clientSession) {
+		this.clientSession = clientSession;
 	}	
 
 }
